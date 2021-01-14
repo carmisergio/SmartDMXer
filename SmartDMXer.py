d4a6c47f9ac8657d93524acf33d6f5fa0abd601a
Sergio Carmine <me@sergiocarmi.net>
2021-01-14 09:46:28 +0100

    Add settings trough config file

--- SmartDMXer.py
+++ SmartDMXer.py
@@ -6,22 +6,22 @@ import json
 import signal
 import time
 import paho.mqtt.client as mqtt
+import yaml
 
-#  CONFIG VARIABLES #
+# DEFAULTS IF NOT IN CONFIG #
 RenderFPS = 60
 RenderChannels = 100
-SubscribeChannels = 100
-BaseTopic = "mansardalight"
-ClientName = "SmartDMXer"
-BrokerHost = "192.168.1.61"
-BrokerPort = 1883
-MqttAuth = True
-MqttUser = "sergio"
-MqttPass = "sergio06"
-SleepTime = 5
-FilePath = "files/lightdata.json"
 DefaultTransition = 1 #s
-# /CONFIG VARIABLES #
+BaseTopic = "smartdmxer"
+ClientName = "SmartDMXer"
+BrokerHost = ""
+BrokerPort = 1883
+MqttAuth = False
+MqttUser = ""
+MqttPass = ""
+FilePath = "files/lightdata.json"
+ConfigPath = "config.yaml"
+# DEFAULTS IF NOT IN CONFIG #
 
 #Init statekeeper arrays 
 haLightBright = []
@@ -32,12 +32,107 @@ FadeTarget = []
 
 mqttfailflag = False
 #Mqtt connection callbacks
+
+def parseConfig():
+    global RenderFPS
+    global RenderChannels
+    global DefaultTransition
+    global BaseTopic
+    global ClientName
+    global BrokerHost
+    global BrokerPort
+    global MqttAuth
+    global MqttUser
+    global MqttPass
+
+    print("INFO: parsing config file...")
+    try:
+        with open(ConfigPath) as file:
+            config = yaml.load(file, Loader=yaml.FullLoader)
+    except Exception as error: 
+        print('ERROR: unable to parse config file. Exiting.')
+        exitprogramnomqtt()
+    if config != None:
+        for key in config:
+            if key == "dmx channels":
+                if isinstance(config[key], int) and 0 < config[key] <= 100:
+                    RenderChannels = config[key]
+                else:
+                    print("ERROR: invalid channel number. Exiting.")
+                    exitprogramnomqtt()
+            elif key == "render fps":
+                if  isinstance(config[key], int) and 0 < config[key] <= 9999:
+                    RenderFPS = config[key]
+                else:
+                    print("ERROR: invalid render FPS. Exiting.")
+                    exitprogramnomqtt()
+            elif key == "default transition":
+                if  isinstance(config[key], int) and 0 < config[key] <= 9999:
+                    DefaultTransition = config[key]
+                else:
+                    print("ERROR: invalid default transition. Exiting.")
+                    exitprogramnomqtt()
+            elif key == "broker host":
+                if isinstance(config[key], str):
+                    BrokerHost = config[key]
+                else:
+                    print("ERROR: invalid broker host. Exiting.")
+                    exitprogramnomqtt()
+            elif key == "broker port":
+                if  isinstance(config[key], int) and 0 < config[key] <= 65535:
+                    BrokerPort = config[key]
+                else:
+                    print("ERROR: invalid broker port. Exiting.")
+                    exitprogramnomqtt()
+            elif key == "client name":
+                if isinstance(config[key], str):
+                    ClientName = config[key]
+                else:
+                    print("ERROR: invalid client name. Exiting.")
+                    exitprogramnomqtt()
+            elif key == "autentication":
+                if isinstance(config[key], bool):
+                    MqttAuth = config[key]
+                else:
+                    print("ERROR: invalid autentication selection, must be true of false. Exiting.")
+                    exitprogramnomqtt()
+            elif key == "user":
+                if isinstance(config[key], str):
+                    MqttUser = config[key]
+                else:
+                    print("ERROR: invalid username. Exiting.")
+                    exitprogramnomqtt()
+            elif key == "password":
+                if isinstance(config[key], str):
+                    MqttPass = config[key]
+                else:
+                    print("ERROR: invalid password. Exiting.")
+                    exitprogramnomqtt()
+            elif key == "base topic":
+                if isinstance(config[key], str):
+                    BaseTopic = config[key]
+                else:
+                    print("ERROR: invalid base topic. Exiting.")
+                    exitprogramnomqtt()
+
+    if BrokerHost == "":
+        print("ERROR: broker host missing. Exiting.")
+        exitprogramnomqtt()
+        
+    if MqttAuth:
+        if MqttPass == "" or MqttUser == "":
+            print("ERROR: mqtt autentication tured on but user or password missing. Exiting.")
+            exitprogramnomqtt()
+        #except:
+        #    print("ERROR: could not parse config file. Exiting.")
+        #    exitprogramnomqtt()
+
 def on_connect(client, userdata, flags, rc):
     print("INFO: MQTT client connected")
-    for i in range(0, SubscribeChannels):
+    for i in range(0, RenderChannels):
         publishLightState(i)
     client.publish(BaseTopic + "/avail" ,"online",qos=0,retain=True)
-    for i in range(0, SubscribeChannels):
+    for i in range(0, RenderChannels):
         client.subscribe(BaseTopic + "/" + str(i) + "/set")
 
 def on_disconnect(client, userdata, rc):
@@ -111,6 +206,15 @@ def publishLightState(lightid):
     jsonData = json.dumps(data)
     client.publish(BaseTopic + "/" + str(lightid) ,jsonData,qos=0,retain=True)
 
+def exitprogram():
+        global client
+        client.publish(BaseTopic + "/avail" ,"offline",qos=0,retain=True)
+        client.loop_stop()
+        outputData = []
+        exit()
+
+def exitprogramnomqtt():
+        exit()
 #Render values from statekeeper arrays to lights
 def renderLights():
     outputData = []
@@ -121,22 +225,14 @@ def renderLights():
         json.dump(data, json_file)
 def main():
     global client
-    def exitprogram():
-        print('ERRROR: user pressed CTRL+C, exiting...')
-        client.publish(BaseTopic + "/avail" ,"offline",qos=0,retain=True)
-        client.loop_stop()
-        outputData = []
-        for _ in range(0, RenderChannels):
-            outputData.append(0)
-        data = {"data": outputData}
-        with open(FilePath, 'w+') as json_file:
-            json.dump(data, json_file)
-        exit()
     #  PUT BANNER print("SmartDMXer DMX engine is starting!")
     def signal_handler(sig, frame):
+        print('ERRROR: user pressed CTRL+C, exiting...')
         exitprogram()
     FPSCLOCK = pygame.time.Clock()
     signal.signal(signal.SIGINT, signal_handler)
+
+    parseConfig()
 
     print("INFO: generating statekeeper arrays...")
 
